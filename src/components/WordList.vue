ce1372e7258951228dfe73585f1746dcf68378eb
Dan Kim <itsdanya@gmail.com>
2020-09-05 19:26:59 +0300

    Create WordList component

---
+++ src/components/WordList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Word list</p>
+
+    <ul>
+      <li>Word 1</li>
+      <li>Word 2</li>
+      <li>Word 3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+    <WordList />
   </div>
 </template>
 
 <script>
+import WordList from '@/components/WordList.vue'
+
 export default {
   name: 'Home',
   components: {
+    WordList
   }
 }
 </script>
