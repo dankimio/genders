bcc576e953c3525dfd19ba861d16ef05107295dd
Dan Kim <itsdanya@gmail.com>
2020-09-05 22:30:04 +0300

    Add buttons for genders

--- src/components/WordList.vue
+++ src/components/WordList.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="h-screen flex items-center justify-center">
+  <div class="h-screen flex items-center justify-center flex-col">
     <div
-      class="p-4 max-w-sm w-full
+      class="p-4 max-w-sm w-full mb-6
         bg-white
         shadow-lg rounded-lg
-        font-serif text-center text-3xl
+        font-serif text-center text-3xl text-gray-900
       "
     >
       <span>{{ currentWord.word }}</span>
-      &nbsp;
-      <span>{{ currentWord.gender }}</span>
+    </div>
+
+    <div class="max-w-sm w-full flex text-gray-800 font-serif">
+      <div class="flex-1 p-3 bg-white shadow-lg rounded-lg text-center">
+        une
+      </div>
+      <div class="mx-2" />
+      <div class="flex-1 p-3 bg-white shadow-lg rounded-lg text-center">
+        un
+      </div>
     </div>
   </div>
 </template>
