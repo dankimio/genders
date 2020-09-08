8411ecda5ef2720094c972a9adfb77eac502b672
Dan Kim <itsdanya@gmail.com>
2020-09-08 20:41:10 +0300

    Extract WordCard component

---
+++ src/components/WordCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="flex items-center justify-center p-4 max-w-sm w-full h-48 mb-6
+        bg-white
+        shadow-lg rounded-lg
+        text-4xl text-gray-900
+      "
+  >
+    <span v-if="incorrectAnswer" class="mr-2 text-red-600">
+      {{ article }}
+    </span>
+
+    <span class="">{{ word.word }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    word: { type: Object, required: true },
+    incorrectAnswer: { type: Boolean, required: true }
+  },
+  computed: {
+    article() {
+      return this.word.gender === 'f' ? 'une' : 'un'
+    }
+  }
+}
+</script>
--- src/components/WordList.vue
+++ src/components/WordList.vue
@@ -4,19 +4,10 @@
       Score: {{ score }}
     </h2>
 
-    <div
-      class="flex items-center justify-center p-4 max-w-sm w-full h-48 mb-6
-        bg-white
-        shadow-lg rounded-lg
-        text-4xl text-gray-900
-      "
-    >
-      <span v-if="incorrectAnswer" class="mr-2 text-red-600">
-        {{ article }}
-      </span>
-
-      <span class="">{{ currentWord.word }}</span>
-    </div>
+    <WordCard
+      :word="currentWord"
+      :incorrect-answer="incorrectAnswer"
+    />
 
     <div class="max-w-sm w-full flex text-gray-800">
       <button
@@ -43,7 +34,10 @@
 </template>
 
 <script>
+import WordCard from './WordCard'
+
 export default {
+  components: { WordCard },
   data () {
     return {
       words: [],
@@ -51,11 +45,6 @@ export default {
       currentWordIndex: 0,
       incorrectAnswer: false,
       score: 0
-    }
-  },
-  computed: {
-    article() {
-      return this.currentWord.gender === 'f' ? 'une' : 'un'
     }
   },
   created () {
