fd84bbc6a4a1edc59b023d58ea5090a77669d1f3
Dan Kim <itsdanya@gmail.com>
2022-12-25 21:01:37 +0300

    Add content option

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
     extend: {},
   },
