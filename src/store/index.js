7a0c37c3e5e4fbe92df7db005e54ea6c582a05c7
Dan Kim <itsdanya@gmail.com>
2022-12-25 18:59:37 +0300

    Remove vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import createPersistedState from 'vuex-persistedstate'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -91,5 +89,5 @@ export default new Vuex.Store({
   },
   modules: {
   },
-  plugins: [createPersistedState()]
+  plugins: []
 })
