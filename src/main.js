f2e99932ce905f9158c476ed6ab423892c48b202
Dan Kim <itsdanya@gmail.com>
2022-12-25 20:30:17 +0300

    Initialize head

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createHead } from "@vueuse/head"
 import App from './App.vue'
 import router from './router'
 
-const pinia = createPinia()
 const app = createApp(App)
+const pinia = createPinia()
+const head = createHead()
 
 app.use(pinia)
+app.use(head)
 app.use(router)
 app.mount('#app')
