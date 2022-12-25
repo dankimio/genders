225942efef0db6b42c20a010beb6e6ff10e02ab4
Dan Kim <itsdanya@gmail.com>
2022-12-25 19:31:30 +0300

    Do not import VueMeta

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import VueMeta from 'vue-meta'
+// import VueMeta from 'vue-meta'
 
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
-Vue.use(VueMeta)
+// Vue.use(VueMeta)
 
 const routes = [
   {
